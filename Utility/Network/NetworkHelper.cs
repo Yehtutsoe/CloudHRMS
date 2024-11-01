272e4c0b4d84bad2ab9728325ce34db593847d83
Yehtutsoe <65958283+Yehtutsoe@users.noreply.github.com>
2024-11-01 19:42:16 +0630

    Update NetworkHelper.cs
    Update

--- Utility/Network/NetworkHelper.cs
+++ Utility/Network/NetworkHelper.cs
@@ -1,37 +1,52 @@
-﻿using System.Net;
+using System;
+using System.Net;
 using System.Net.Sockets;
+using Microsoft.Extensions.Caching.Memory;
 
-namespace CloudHRMS.Utility.Network
+namespace CloudHRMS.Utility.NetworkHelper
 {
-	public static class NetworkHelper
-	{
-		public static string GetMechinePublicIP()
-		{
-			string ip = "127.0.0.1";
-			try
-			{
-				ip =new System.Net.WebClient().DownloadString("https://api.ipify.org");
+    public static class NetworkHelper
+    {
+        private static readonly TimeSpan CacheExpiration = TimeSpan.FromMinutes(10);
 
-			}catch(Exception e)
-			{
-				ip=GetLocalIPAddress();	
-			}
+        // Get Public IP of Machine (cached)
+        public static string GetMachinePublicIP(IMemoryCache cache)
+        {
+            string cacheKey = "MachinePublicIP";
+            
+            // Check if IP address is already cached
+            if (!cache.TryGetValue(cacheKey, out string publicIP))
+            {
+                // If not cached, retrieve Public IP and cache it
+                publicIP = "127.0.0.1";
+                try
+                {
+                    publicIP = new WebClient().DownloadString("https://api.ipify.org");
+                }
+                catch (Exception)
+                {
+                    publicIP = GetLocalIPAddress();
+                }
 
-			return ip;
-		}
+                // Cache the Public IP for the specified duration
+                cache.Set(cacheKey, publicIP, CacheExpiration);
+            }
 
-		public static string GetLocalIPAddress()
-		{
-			
-			var host = Dns.GetHostEntry(Dns.GetHostName());
-			foreach( var ip in host.AddressList)
-			{
-				if(ip.AddressFamily == AddressFamily.InterNetwork)
-				{
-					return ip.ToString();
-				}
-			}
-			throw new Exception("No Network adapter");
-		}
-	}
+            return publicIP;
+        }
+
+        // Get Local IP of Machine
+        public static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
+    }
 }
