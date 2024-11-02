b53d3c61e909562f0b0d17ed63aecfede243ed51
YeHtutSoe <yeandzarforever@gmail.com>
2024-11-01 20:19:25 -0700

    Initial

--- Models/Entities/Shift.cs
+++ Models/Entities/Shift.cs
@@ -1,6 +1,16 @@
-﻿namespace CloudHRMS.Models.Entities
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CloudHRMS.Models.Entities
 {
-    public class Shift
+    public class Shift:BaseEntity
     {
+        public string Name { get; set; }
+        public TimeSpan InTime { get; set; }
+        public TimeSpan OutTime { get; set; }
+        public TimeSpan LateAfter { get; set; }
+        public TimeSpan EarlyOutBefore { get; set; }
+        public string AttendancePolicyId { get; set; }
+        [ForeignKey(nameof(AttendancePolicyId))]
+        public AttendancePolicyEntity AttendancePolicy { get; set; }
     }
 }
---
+++ Models/ViewModels/ShiftViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace CloudHRMS.Models.ViewModels
+{
+    public class ShiftViewModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public TimeSpan InTime { get; set; }
+        public TimeSpan OutTime { get; set; }
+        public TimeSpan LateAfter { get; set; }
+        public TimeSpan EarlyOutBefore { get; set; }
+    }
+}
