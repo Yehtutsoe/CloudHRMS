b78d244db7edaff4c5bb1ca1bed7e6b1ac45f39f
YeHtutSoe <yeandzarforever@gmail.com>
2024-11-24 21:43:19 +0630

    Update

--- Models/ViewModels/ErrorViewModel.cs
+++ Models/ViewModels/ErrorViewModel.cs
@@ -2,8 +2,6 @@ namespace CloudHRMS.Models.ViewModels
 {
     public class ErrorViewModel
     {
-
-
         public string Message { get; set; }
         public bool IsOccurError { get; set; } = false;
     }
